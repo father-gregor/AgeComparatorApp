b65a1f34053c42abd835db4abc253cb392900e68
father-gregor <benlinus92@gmail.com>
2017-07-25 18:46:17 +0300

    Add link to routes

--- app.js
+++ app.js
@@ -1,6 +1,44 @@
-/**
- * Created by puzomen on 24.07.2017.
- */
-
+//Initialize
+var http = require("http");
 var express = require("express");
+var mongoose = require("mongoose");
+var routes = require("./routes");
+var user = require("./routes/user");
+var path = require("path");
+var database = require("./config/database");
+
+var favicon = require("serve-favicon");
+var morganLog = require("morgan");
+var methodOverride = require("method-override");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var multer = require("multer");
+var errorHandler = require("errorhandler");
+
 var app = express();
+
+// Add environments variables
+mongoose.connect(database.url);
+app.set("port", process.env.PORT || 3000);
+app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(morganLog("dev"));
+app.use(methodOverride());
+app.use(session({ resave: true,
+    saveUninitialized: true,
+    secret: "uwotm8" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+app.use(express.static(path.join(__dirname, "public")));
+
+require("./server/routes")();
+
+// error handling middleware should be loaded after the loading the routes
+if ("development" == app.get("env")) {
+    app.use(errorHandler());
+}
+
+var server = http.createServer(app);
+server.listen(app.get("port"), function(){
+    console.log("Listening on port " + app.get("port"));
+});
