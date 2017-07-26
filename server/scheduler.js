c90044b9b31fc401e1d6e2f78892ddd2fda67dcc
father-gregor <benlinus92@gmail.com>
2017-07-27 00:43:53 +0300

    Add scheduler task to execute every 2 minutes

---
+++ server/scheduler.js
@@ -0,0 +1,22 @@
+var scheduler = require("node-schedule");
+var User = require("./models/user");
+var deleteOldestTask = function() {
+    var startTime = new Date(Date.now() + 5000); //start after 5 seconds
+    var scheduleObj = scheduler.scheduleJob({start: startTime, rule: "*/1 * * * *"}, function() {
+        User.findOne({}).sort("-age").exec(function(err, user) {
+            console.log("Removing oldest user task executed at " + new Date(Date.now()).toString());
+            if(err) {
+                console.log("Error while finding oldest user: " + err);
+            } else {
+                User.remove(user, function(err) {
+                    if(err)
+                        console.log("Error while removing oldest user: " + err);
+                });
+            }
+        })
+    });
+}
+
+module.exports = {
+    deleteOldestTask
+};
