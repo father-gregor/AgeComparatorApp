a64e1c75d52d24fea8b4f040f734ada23b24569e
father-gregor <benlinus92@gmail.com>
2017-07-27 03:15:28 +0300

    Add error span fields for inputs

--- public/js/userFormCtrl.js
+++ public/js/userFormCtrl.js
@@ -2,12 +2,16 @@ app.controller("userFormCtrl", function($scope, $http, $rootScope) {
     $scope.user = {};
     $scope.genders = ["Male", "Female"];
     $scope.sendUserForm = function() {
-        $http.post("/api/add-user", $scope.user)
-            .then(function success(resp) {
-                $rootScope.$broadcast("updatedUserArray", resp.data);
-            },
-            function error(err) {
-                console.log("Data submit error: " + err);
-            });
+        console.log("Form " + $scope.userform.$valid + " name " + $scope.userform.name.$valid + " surname " + $scope.userform.surname.$valid
+            + " gender " + $scope.userform.gender.$valid + " age " + $scope.userform.age.$valid + " email " + $scope.userform.email.$valid);
+        if($scope.userform.$valid && $scope.userform.$dirty) {
+            $http.post("/api/add-user", $scope.user)
+                .then(function success(resp) {
+                        $rootScope.$broadcast("updatedUserArray", resp.data);
+                    },
+                    function error(err) {
+                        console.log("Data submit error: " + err);
+                    });
+        }
     }
 });
