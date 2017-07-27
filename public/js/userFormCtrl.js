d78eaea14367f5bc056ad8f5fbbc4a81cea65ca0
father-gregor <benlinus92@gmail.com>
2017-07-27 03:19:46 +0300

    Minor fixes

--- public/js/userFormCtrl.js
+++ public/js/userFormCtrl.js
@@ -2,8 +2,6 @@ app.controller("userFormCtrl", function($scope, $http, $rootScope) {
     $scope.user = {};
     $scope.genders = ["Male", "Female"];
     $scope.sendUserForm = function() {
-        console.log("Form " + $scope.userform.$valid + " name " + $scope.userform.name.$valid + " surname " + $scope.userform.surname.$valid
-            + " gender " + $scope.userform.gender.$valid + " age " + $scope.userform.age.$valid + " email " + $scope.userform.email.$valid);
         if($scope.userform.$valid && $scope.userform.$dirty) {
             $http.post("/api/add-user", $scope.user)
                 .then(function success(resp) {
